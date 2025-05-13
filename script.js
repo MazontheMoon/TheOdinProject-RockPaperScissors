3a76862b28b0ce94efa8b392988a492caeee2721
Mary Ronan <36551329+MazontheMoon@users.noreply.github.com>
2025-05-13 15:24:14 +0100

    update: Add button based game play with scoreboard
    Conversion from console to GUI input complete with scoreboard feature to time 5 rounds per game

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
 let humanScore = 0;
 let computerScore = 0;
-let roundCounter = 0;
 let humanChoice;
 let computerChoice;
+
 const btnRock = document.getElementById("btn-rock");
 const btnPaper = document.getElementById("btn-paper");
 const btnScissors = document.getElementById("btn-scissors");
@@ -14,91 +14,95 @@ btnRock.addEventListener("click", () => playRound("ROCK"));
 btnPaper.addEventListener("click", () => playRound("PAPER"));
 btnScissors.addEventListener("click", () => playRound("SCISSORS"));
 
-function getComputerChoice() {
-  let choices = ["ROCK", "PAPER", "SCISSORS"];
-  return choices[Math.floor(Math.random() * 3)];
-}
-
 function playRound(humanChoice) {
   computerChoice = getComputerChoice();
+  score.textContent = "";
   while (humanChoice == "ROCK") {
     if (computerChoice == "ROCK") {
-      console.log(`${humanChoice} draws with ${computerChoice}`);
-      score.textContent += `${roundCounter}: ${humanChoice} draws with ${computerChoice}`;
-      scoreComputer.textContent += computerScore;
-      scorePlayer.textContent += humanScore;
+      score.textContent += `${humanChoice} draws with ${computerChoice}`;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     } else if (computerChoice == "PAPER") {
-      console.log(`${humanChoice} loses to ${computerChoice}`);
       score.textContent = `${humanChoice} draws with ${computerChoice}`;
       computerScore++;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     } else {
-      console.log(`${humanChoice} beats ${computerChoice}`);
       score.textContent = `${humanChoice} draws with ${computerChoice}`;
       humanScore++;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     }
   }
   while (humanChoice == "PAPER") {
     if (computerChoice == "PAPER") {
-      console.log(`${humanChoice} draws with ${computerChoice}`);
       score.textContent = `${humanChoice} draws with ${computerChoice}`;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     } else if (computerChoice == "SCISSORS") {
-      console.log(`${humanChoice} loses to ${computerChoice}`);
       score.textContent = `${humanChoice} loses to ${computerChoice}`;
       computerScore++;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     } else {
-      console.log(`${humanChoice} beats ${computerChoice}`);
       score.textContent = `${humanChoice} loses to ${computerChoice}`;
       humanScore++;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     }
   }
   while (humanChoice == "SCISSORS") {
     if (computerChoice == "SCISSORS") {
-      console.log(`${humanChoice} draws with ${computerChoice}`);
       score.textContent = `${humanChoice} draws with ${computerChoice}`;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     } else if (computerChoice == "ROCK") {
-      console.log(`${humanChoice} loses to ${computerChoice}`);
       score.textContent = `${humanChoice} loses to ${computerChoice}`;
       computerScore++;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     } else {
-      console.log(`${humanChoice} beats ${computerChoice}`);
       score.textContent = `${humanChoice} beats ${computerChoice}`;
       humanScore++;
+      scoreComputer.textContent = computerScore;
+      scorePlayer.textContent = humanScore;
       break;
     }
   }
-  roundCounter++;
+
+  if (humanScore == 5) {
+    score.textContent = "Player wins Game!";
+    humanScore = 0;
+    computerScore = 0;
+    scoreComputer.textContent = computerScore;
+    scorePlayer.textContent = humanScore;
+  }
+
+  if (computerScore == 5) {
+    score.textContent = "Computer wins Game!";
+    humanScore = 0;
+    computerScore = 0;
+    scoreComputer.textContent = computerScore;
+    scorePlayer.textContent = humanScore;
+  }
 }
 
-if (roundCounter >= 5) {
-  roundCounter = 0;
-  humanScore = 0;
-  computerScore = 0;
-}
-/*
-function playGame () {
-  for(let i = 1; i <=5; i++) {
-		humanChoice = getHumanChoice();
-		computerChoice = getComputerChoice();
-		playRound(humanChoice, computerChoice);
-    console.log(`Round ${i} Player score: ${humanScore}`);
-    console.log(`Round ${i} Computer score: ${computerScore}`);
-	}
-  humanScore = 0;
-  computerScore = 0;
+function getComputerChoice() {
+  let choices = ["ROCK", "PAPER", "SCISSORS"];
+  return choices[Math.floor(Math.random() * 3)];
 }
 
-playGame();
-*/
-
-/* CONSOLE ONLY VERSION
+/***********************
+* CONSOLE ONLY VERSION *
+************************
 
 let humanScore = 0;
 let computerScore = 0;
